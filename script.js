c4c0e3abac12d28cd3b9a8b4fbdc1ee976bab7d5
jvmcpheron <janemcpheron@gmail.com>
2023-10-09 16:36:38 -0500

    fixed up pancake menu

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const pancakeMenuToggle = document.querySelector('.pancake-menu-toggle');
-const pancakeMenu = document.querySelector('.pancake-menu');
+const pancakeMenu = document.querySelector('.pancake-display-none');
+const pancakeExit = document.querySelector('.pancake-menu-exit');
+
+
 
 pancakeMenuToggle.addEventListener('click', () => {
-    pancakeMenu.classList.toggle('show-pancake-menu');
+    pancakeMenu.style.display = "flex";
+    pancakeMenuToggle.style.display = "none";
+    pancakeExit.style.display = "flex";
 });
 
+pancakeExit.addEventListener('click', () => {
+    pancakeMenu.style.display = "none";
+    pancakeMenuToggle.style.display = "flex";
+    pancakeExit.style.display = "none";
+});
